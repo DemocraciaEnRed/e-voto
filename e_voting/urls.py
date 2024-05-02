84f2d21e14c4a6e358ba3c91095d24620578baf1
Pano Valdez <marianovaldez92@protonmail.com>
2024-05-02 17:42:09 -0300

    staticfiles

--- e_voting/urls.py
+++ e_voting/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import settings
 
 urlpatterns = [
@@ -24,4 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('administrator/', include('administrator.urls')),
     path('voting/', include('voting.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + staticfiles_urlpatterns()
